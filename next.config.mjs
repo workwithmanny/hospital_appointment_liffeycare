7062b178f9f92d1ca3b56666c0ee83e2ac1b1c31
Manny Developer <manny@example.com>
2026-01-27 09:00:00 -0800

    Add Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb"
+    }
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+};
+
+export default nextConfig;
